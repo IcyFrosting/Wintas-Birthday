df2ea49167c9ccd395e6659a55d68ecf9b401408
IcyFrosting <101979015+IcyFrosting@users.noreply.github.com>
2022-03-20 17:49:04 +0000

    Delete winta birthday directory

--- winta birthday/assets/javascript.js
+++
@@ -1,8 +0,0 @@
-var cards = document.getElementById('allCards')
-var start = document.getElementById('start')
-var but = document.getElementById('button')
-
-but.addEventListener('click', function () {
-    start.setAttribute('style', 'display:none')
-    cards.setAttribute('style', 'display:block')
-})
